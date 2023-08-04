762faf3a45cc2148eb8c204cc4c6368842f8cae3
AbhiGithub123 <abhishekkanuganti79@gmail.com>
2023-08-04 12:53:36 -0400

    namaste react episode 01

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading=React.createElement("div",{id:"parent"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]);
+    console.log(heading);
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
